541e7de402973bf74dee77c98e044b4d3095fb0a
aldottexe <aldottexe@gmail.com>
2024-09-26 12:03:23 -0400

    final commit

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -18,9 +18,10 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 4)
 
         // TODO (Step 2: Define lambda to modify displayTextView size)
+        val resizeTextView = {textSize : Float -> displayTextView.textSize = textSize}
 
         // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
+        recyclerView.adapter = NumberDisplayAdapter(numbers, resizeTextView)
 
 
     }
